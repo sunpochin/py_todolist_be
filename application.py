e81f2afe68f35433b75543935d98bd6763098f23
sun pochin <sunpochin@gmail.com>
2021-09-17 21:57:27 +0800

    change to sqlalquemy

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
-import MySQLdb
+import sqlalchemy
 
 load_dotenv()
 
@@ -38,8 +38,82 @@ application = Flask(__name__)
 application.add_url_rule('/<username>', 'hello', (lambda username:
     header_text + say_hello(username) + home_link + footer_text))
 
+
+def init_connection_engine():
+    db_config = {
+        # [START cloud_sql_mysql_sqlalchemy_limit]
+        # Pool size is the maximum number of permanent connections to keep.
+        "pool_size": 5,
+        # Temporarily exceeds the set pool_size if no connections are available.
+        "max_overflow": 2,
+        # The total number of concurrent connections for your application will be
+        # a total of pool_size and max_overflow.
+        # [END cloud_sql_mysql_sqlalchemy_limit]
+
+        # [START cloud_sql_mysql_sqlalchemy_backoff]
+        # SQLAlchemy automatically uses delays between failed connection attempts,
+        # but provides no arguments for configuration.
+        # [END cloud_sql_mysql_sqlalchemy_backoff]
+
+        # [START cloud_sql_mysql_sqlalchemy_timeout]
+        # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
+        # new connection from the pool. After the specified amount of time, an
+        # exception will be thrown.
+        "pool_timeout": 30,  # 30 seconds
+        # [END cloud_sql_mysql_sqlalchemy_timeout]
+
+        # [START cloud_sql_mysql_sqlalchemy_lifetime]
+        # 'pool_recycle' is the maximum number of seconds a connection can persist.
+        # Connections that live longer than the specified amount of time will be
+        # reestablished
+        "pool_recycle": 1800,  # 30 minutes
+        # [END cloud_sql_mysql_sqlalchemy_lifetime]
+
+    }
+
+    if os.environ.get("DB_HOST"):
+        return init_tcp_connection_engine(db_config)
+    # else:
+    #     return init_unix_connection_engine(db_config)
+
+def init_tcp_connection_engine(db_config):
+    # [START cloud_sql_mysql_sqlalchemy_create_tcp]
+    # Remember - storing secrets in plaintext is potentially unsafe. Consider using
+    # something like https://cloud.google.com/secret-manager/docs/overview to help keep
+    # secrets secret.
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_host = os.environ["DB_HOST"]
+
+    # Extract host and port from db_host
+    host_args = db_host.split(":")
+    db_hostname, db_port = host_args[0], int(host_args[1])
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+        sqlalchemy.engine.url.URL.create(
+            drivername="mysql+pymysql",
+            username=db_user,  # e.g. "my-database-user"
+            password=db_pass,  # e.g. "my-database-password"
+            host=db_hostname,  # e.g. "127.0.0.1"
+            port=db_port,  # e.g. 3306
+            database=db_name,  # e.g. "my-database-name"
+        ),
+        **db_config
+    )
+    # [END cloud_sql_mysql_sqlalchemy_create_tcp]
+
+    return pool
+
+db = None
+
 def getDances():
     dances = []
+    global db
+    db = db or init_connection_engine()
+
     # db = MySQLdb.connect(host=RDS_HOSTNAME,
     #     port=3306,
     #     user=RDS_USERNAME,
@@ -52,10 +126,14 @@ def getDances():
     # query = """SELECT * FROM dances;"""
     # cursor.execute(query)
     # dances = cursor.fetchall()
+    with db.connect() as conn:
+        dances = conn.execute(
+            """SELECT * FROM dances;"""
+        )
 
-    # # Convert it into a list for readability
-    # dances = [list(l) for l in dances]
-    # print('get Dances from mysql DB: ', dances)
+    # Convert it into a list for readability
+    dances = [list(l) for l in dances]
+    print('get Dances from mysql DB: ', dances)
     return dances
 
 
